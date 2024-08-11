866ca356490278231cdf2663fd37c694dcc97b26
SantiYami <davidgarces.98@gmail.com>
2024-08-11 12:51:16 -0500

    Changes to get_file_info to support single files and multiple files

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -1,22 +1,39 @@
 import os
-from app.handlers.factory import DocumentHandlerFactory
+from app.handlers.base.document_handler_factory import DocumentHandlerFactory
 
-def get_file_info(directory):
+def get_file_info(paths, factory=None):
+    if factory is None:
+        factory = DocumentHandlerFactory()
+
     document_info = []
     total_size = 0
     file_count = 0
 
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_extension = os.path.splitext(file)[1].lower()
-            handler = DocumentHandlerFactory.get_handler(file_extension)
+    if isinstance(paths, str):
+        files = _get_files_from_directory(paths)
+    elif isinstance(paths, list):
+        files = paths
+    else:
+        raise ValueError("Invalid type for paths. Expected str or list.")
 
-            if handler:
-                info = handler.get_info(file_path)
-                if info:
-                    document_info.append(info)
-                    total_size += os.path.getsize(file_path)
-                    file_count += 1
+    for file_path in files:
+        file_extension = os.path.splitext(file_path)[1].lower()
+        try:
+            handler = factory.get_handler(file_extension)
+            info = handler.get_info(file_path)
+            if info:
+                document_info.append(info)
+                total_size += info["size"]
+                file_count += 1
+        except ValueError as e:
+            print(f"Error: {e}")
 
     return document_info, total_size, file_count
+
+def _get_files_from_directory(directory):
+    """Helper function to retrieve all files in a directory recursively."""
+    files = []
+    for root, dirs, file_names in os.walk(directory):
+        for file_name in file_names:
+            files.append(os.path.join(root, file_name))
+    return files
