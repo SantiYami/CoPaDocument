c395128598e82d89d70a5e4a8bf4709129fa9cc6
SantiYami <davidgarces.98@gmail.com>
2024-08-11 12:48:23 -0500

    Changes PDFHandler logic and folder movement

--- app/handlers/pdf/pdf_handler.py
+++ app/handlers/pdf/pdf_handler.py
@@ -1,21 +1,19 @@
-import os
 from PyPDF2 import PdfReader
-from .document_handler import DocumentHandler
-from ..utils.format_utils import format_size
+from ..base.document_handler import DocumentHandler
 
 class PDFHandler(DocumentHandler):
 
     def get_info(self, file_path):
         try:
+            file_info = self.extract_file_info(file_path)
             with open(file_path, 'rb') as f:
                 reader = PdfReader(f)
                 num_pages = len(reader.pages)
-                size = os.path.getsize(file_path)
-                return {
-                    "path": file_path,
-                    "pages": num_pages,
-                    "size": format_size(size)
-                }
+            
+            file_info.update({
+                "pages": num_pages
+            })
+            return file_info
         except Exception as e:
             print(f"Error processing {file_path}: {e}")
             return None
